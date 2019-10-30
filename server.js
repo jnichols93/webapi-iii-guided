8e4484690c0ea89aa516cf14a0c0cbe51a9fd043
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-10-30 12:29:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
-
+const morgan = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
@@ -14,15 +14,30 @@ console.log(new Date().toISOString());
 }
 
 function logger(req,res,next){
-console.log(`[${new Date().toISOString()}${req.method} to ${req.url}`);
+console.log(`[${new Date().toISOString()}]${req.method} to ${req.url}`);
 
   next();
 }
+
+//* change the gatekeeper to return a 400 if no password is provided and a message that says please proved a password
+//* if a password is provided and it is mellon,call next, otherwise return a 401
+//* and the "you: shall not pass"
+function gateKeeper(req,res,next){
+  // data can come in through the body, url, parameters, query string, 
+  //new way of reading data sent by client
+  const password = req.headers.password;
+  if(password.toLowerCase() ==='melon') {
+    next();
+  } else{
+    res.status(400).json({you:'cannot pass!!'});
+  }
+}
 //Global Middleware
 server.use(helmet()); //third-party
+server.use(gateKeeper);
 server.use(express.json()); /// Built-in
 server.use(dateLogger); //custom middleware
-
+server.use(morgan('dev'));
 server.use('/api/hubs', hubsRouter);
 
 
